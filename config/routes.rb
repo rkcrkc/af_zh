65cbf0815be368cfc49331569543fbfbd8306ee8
RyanCrocombe <ryancrocombe@gmail.com>
2017-12-14 13:30:52 +0800

    Add mailform and email css

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  resources :contacts, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
